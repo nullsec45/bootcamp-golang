3fdd71260b58e43c0d0ec9d63488bdf741b7288e
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2026-01-31 22:03:05 +0700

    feat : layered architecture.

---
+++ kasir-api/database/database.go
@@ -0,0 +1,28 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"fmt"
+)
+
+func InitDB(connectionString string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", connectionString)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("Database connection error:", err)
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(25)
+
+	log.Println("Database connected successfully")
+	return db, nil
+}
--- kasir-api/main.go
+++ kasir-api/main.go
@@ -6,6 +6,13 @@ import (
 	"encoding/json"
 	"strings"
 	"strconv"
+	"kasir-api/database"
+	"kasir-api/handlers"
+	"os"
+	"log"
+	"kasir-api/repositories"
+	"kasir-api/services"
+	"github.com/spf13/viper"
 )
 
 type Product struct {
@@ -33,6 +40,11 @@ var category=[]Category{
 	{ID:2, Name:"Bumbu Dapur", Description:"Aneka Bumbu Dapur"},
 	{ID:3, Name:"Obat", Description:"Obat-obatan untuk sakit apapun, termasuk sakit hati."},
 } 
+
+type Config struct {
+	Port    string `mapstructure:"PORT"`
+	DBConn string `mapstructure:"DB_CONN"`
+}
 
 func getProductByID(w http.ResponseWriter, r *http.Request){
 		idStr := strings.TrimPrefix(r.URL.Path, "/api/product/")
@@ -184,80 +196,115 @@ func deleteCategory(w http.ResponseWriter, r *http.Request){
 
 
 func main(){
-	http.HandleFunc("/api/product/", func(w http.ResponseWriter, r *http.Request){
-		if r.Method == "GET" {
-			getProductByID(w, r)
-		}else if r.Method == "PUT" {
-			updateProduct(w, r)
-		}else if r.Method == "DELETE" {
-			deleteProduct(w, r)
-		}
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	if _, err := os.Stat(".env"); err == nil {
+		viper.SetConfigFile(".env")
+		_ = viper.ReadInConfig()
+	}
+
+	config := Config{
+		Port: viper.GetString("PORT"),
+		DBConn: viper.GetString("DB_CONN"),
+	}
+
+	fmt.Println("Config:",config.DBConn)
+
+	
+	
+	// http.HandleFunc("/api/product/", func(w http.ResponseWriter, r *http.Request){
+	// 	if r.Method == "GET" {
+	// 		getProductByID(w, r)
+	// 	}else if r.Method == "PUT" {
+	// 		updateProduct(w, r)
+	// 	}else if r.Method == "DELETE" {
+	// 		deleteProduct(w, r)
+	// 	}
 		
-	})
+	// })
 
-	http.HandleFunc("/api/product", func(w http.ResponseWriter, r *http.Request){
-		if r.Method == "GET" {
-			w.Header().Set("Content-Type","application/json")
-			json.NewEncoder(w).Encode(product)
-		}else if r.Method == "POST"{
-			var newProduct Product
-			err := json.NewDecoder(r.Body).Decode(&newProduct)
-			if err != nil {
-				http.Error(w, "Invalid request", http.StatusBadRequest)
-			}
+	// http.HandleFunc("/api/product", func(w http.ResponseWriter, r *http.Request){
+	// 	if r.Method == "GET" {
+	// 		w.Header().Set("Content-Type","application/json")
+	// 		json.NewEncoder(w).Encode(product)
+	// 	}else if r.Method == "POST"{
+	// 		var newProduct Product
+	// 		err := json.NewDecoder(r.Body).Decode(&newProduct)
+	// 		if err != nil {
+	// 			http.Error(w, "Invalid request", http.StatusBadRequest)
+	// 		}
 
-			newProduct.ID=len(product) + 1
-			product=append(product, newProduct)
+	// 		newProduct.ID=len(product) + 1
+	// 		product=append(product, newProduct)
 
-			w.Header().Set("Content-Type","application/json")
-			w.WriteHeader(http.StatusCreated)
-			json.NewEncoder(w).Encode(newProduct)
-		}
-	})
+	// 		w.Header().Set("Content-Type","application/json")
+	// 		w.WriteHeader(http.StatusCreated)
+	// 		json.NewEncoder(w).Encode(newProduct)
+	// 	}
+	// })
 
-	http.HandleFunc("/api/categories/", func(w http.ResponseWriter, r *http.Request){
-		if r.Method == "GET" {
-			getCategoryByID(w, r)
-		}else if r.Method == "PUT" {
-			updateCategory(w, r)
-		}else if r.Method == "DELETE" {
-			deleteCategory(w, r)
-		}
+	// http.HandleFunc("/api/categories/", func(w http.ResponseWriter, r *http.Request){
+	// 	if r.Method == "GET" {
+	// 		getCategoryByID(w, r)
+	// 	}else if r.Method == "PUT" {
+	// 		updateCategory(w, r)
+	// 	}else if r.Method == "DELETE" {
+	// 		deleteCategory(w, r)
+	// 	}
 		
-	})
+	// })
 
-	http.HandleFunc("/api/categories", func(w http.ResponseWriter, r *http.Request){
-		if r.Method == "GET" {
-			w.Header().Set("Content-Type","application/json")
-			json.NewEncoder(w).Encode(category)
-		}else if r.Method == "POST"{
-			var newCategory Category
-			err := json.NewDecoder(r.Body).Decode(&newCategory)
-			if err != nil {
-				http.Error(w, "Invalid request", http.StatusBadRequest)
-			}
+	// http.HandleFunc("/api/categories", func(w http.ResponseWriter, r *http.Request){
+	// 	if r.Method == "GET" {
+	// 		w.Header().Set("Content-Type","application/json")
+	// 		json.NewEncoder(w).Encode(category)
+	// 	}else if r.Method == "POST"{
+	// 		var newCategory Category
+	// 		err := json.NewDecoder(r.Body).Decode(&newCategory)
+	// 		if err != nil {
+	// 			http.Error(w, "Invalid request", http.StatusBadRequest)
+	// 		}
 
-			newCategory.ID=len(category) + 1
-			category=append(category, newCategory)
+	// 		newCategory.ID=len(category) + 1
+	// 		category=append(category, newCategory)
 
-			w.Header().Set("Content-Type","application/json")
-			w.WriteHeader(http.StatusCreated)
-			json.NewEncoder(w).Encode(newCategory)
-		}
-	})
+	// 		w.Header().Set("Content-Type","application/json")
+	// 		w.WriteHeader(http.StatusCreated)
+	// 		json.NewEncoder(w).Encode(newCategory)
+	// 	}
+	// })
 
-	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request){
-		w.Header().Set("Content-Type","application/json")
-		json.NewEncoder(w).Encode(map[string]string{
-			"status":"OK",
-			"message":"API Running",
-		})
-	})
+	// http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request){
+	// 	w.Header().Set("Content-Type","application/json")
+	// 	json.NewEncoder(w).Encode(map[string]string{
+	// 		"status":"OK",
+	// 		"message":"API Running",
+	// 	})
+	// })
 
-	fmt.Println("Server running on localhost:8080")
 
-	err := http.ListenAndServe(":8080",nil)
+	db, err := database.InitDB(config.DBConn)
 	if err != nil {
-		fmt.Println("Gagal running server")
+		log.Fatal("Failed to initialize database:", err)
+	}
+
+	defer db.Close()
+
+	productRepo := repositories.NewProductRepository(db)
+	productService := services.NewProductService(productRepo)
+	productHandler := handlers.NewProductHandler(productService)
+
+	http.HandleFunc("/api/product", productHandler.HandleProducts)
+	http.HandleFunc("/api/product/", productHandler.HandleProductByID) 
+
+	addr := "0.0.0.0:" + config.Port
+
+	fmt.Println("Server running on port", config.Port)
+
+	err = http.ListenAndServe(addr,nil)
+
+	if err != nil {
+		fmt.Println("Failed running server")
 	}
 }
